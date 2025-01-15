4c6b5d3d0e1e29421e22ea7f73fef4e986a62c2a
opilane <opilane@tpt.lan>
2025-01-15 13:42:42 +0200

    simple-pagination

--- resources/views/partials/simple-pagination.blade.php
+++ resources/views/partials/simple-pagination.blade.php
@@ -1,19 +1,31 @@
 @if ($paginator->hasPages())
     <nav>
         <ul class="pagination">
+            {{-- First Page Link --}}
+            @if ($paginator->onFirstPage())
+                <button class="disabled join-item btn btn-outline">«</button>
+            @else
+                <a href="/?page=1"><button class="join-item btn btn-outline">«</button></a>
+            @endif
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
+                <li class="disabled join-item btn btn-outline" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
             @else
-                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+                <li class="join-item btn btn-outline"><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
             @endif
-
+            {{-- Current Page --}}
+            <button class="disabled join-item btn">{{$paginator->currentPage()}}</button>
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+                <li class="join-item btn btn-outline"><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
             @else
-                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+                <li class="disabled join-item btn btn-outline" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+            @endif
+            {{-- Last Page Link --}}
+            @if ($paginator->hasMorePages())
+                <a href="/?page={{round(1000/16)}}"><button class="join-item btn btn-outline">»</button></a>
+            @else
+                <button class="disabled join-item btn btn-outline">»</button>
             @endif
         </ul>
     </nav>
-@endif
