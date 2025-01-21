e1aa4986cec00a151d526bf8077be736040bcf46
djatsenko <vladislav.djatsenko@tptlive.ee>
2025-01-21 17:59:55 +0200

    profile and reset-pass pages rewrite in daisyui

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,44 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
+@extends('partials.layout')
+@section('title', 'Email Verification')
+@section('content')
+    <div class="container mx-auto">
+        <div class="card bg-base-300 shadow-xl w-1/2 mx-auto">
+            <div class="card-body">
+                <div class="mb-4 text-sm text-white-600">
+                    <p class="text-white">Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.</p>
+                </div>
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
+    
+                @if (session('status') == 'verification-link-sent')
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        <p class="text-white">A new verification link has been sent to the email address you provided during registration.</p>
+                    </div>
+                @endif
 
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+    
+                <div class="mt-4 flex items-center justify-between">
+                    <form method="POST" action="{{ route('verification.send') }}">
+                        @csrf
+
+            
+                        <button type="submit" class="btn btn-primary">
+                            <p class="text-white">Resend Verification Email</p>
+                        </button>
+                    </form>
+
+        
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+            
+                        <button type="submit" class="btn btn-secondary btn-error">
+                            <p class="text-white">Log Out</p>
+                        </button>
+                    </form>
+                </div>
             </div>
-        </form>
-
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
-            </button>
-        </form>
+        </div>
     </div>
-</x-guest-layout>
+
+@endsection
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,46 +1,57 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Profile Information') }}
+       
+        <h2 class="mt-4 text-lg font-medium text-white-600">
+            <p class="text-white">Profile Information</p>
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your account's profile information and email address.") }}
+        
+        <p class="mt-4 text-sm text-white-600">
+            <p class="text-white">Update your account's profile information and email address</p>
         </p>
     </header>
 
-    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
-        @csrf
-    </form>
-
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
-        @csrf
+@@ -18,46 +18,49 @@
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            
+            <label for="name" class="text-white block">Name</label>
+            <input id="name" name="name" type="text"
+                class="mt-1 block w-full input input-bordered @error('name') input-error @enderror"
+                value="{{ old('name', $user->name) }}" required autofocus autocomplete="name" />
+            @error('name')
+                <span class="text-error mt-2 text-sm">{{ $message }}</span>
+            @enderror
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
+            
+            <label for="email" class="text-white">Email</label>
+            <input id="email" name="email" type="email" class="mt-1 block w-full input input-bordered"
+                value="{{ old('email', $user->email) }}" required autofocus autocomplete="username" />
+            @error('email')
+                <span class="text-error mt-2 text-sm">{{ $message }}</span>
+            @enderror
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+            
+            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
                 <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
+                   
+                    <p class="text-sm mt-2 text-gray-300">
+                        Your email address is unverified.
 
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
+                        
+                        <button form="send-verification"
+                            class="underline text-sm text-gray-300 hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            Click here to re-send the verification email.
                         </button>
                     </p>
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
+                            
+                            A new verification link has been sent to your email address.
                         </p>
                     @endif
                 </div>
@@ -48,17 +59,13 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+          
+            <button type="subtim" class="btn btn-primary">Save</button>
 
             @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
+                
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-300">Saved</p>
             @endif
         </div>
     </form>
-</section>
