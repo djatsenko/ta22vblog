a831c208328ca73c3cbc4a23c1c5409e5d88193f
djatsenko <vladislav.djatsenko@tptlive.ee>
2025-01-24 15:39:01 +0200

    add user CRUD

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,17 +1,18 @@
-@extends('partials.layout')
-@section('title', $post->title)
-@section('content')
-    <div class="container mx-auto">
-        @include('partials.post-card')
-        <h3 class="text-2xl">Comments:</h3>
-        @foreach($post->comments()->latest()->get() as $comment)
-            <div class="card bg-base-200 shadow-xl mt-3">
-                <div class="card-body">
-                    <p>{{ $comment->body }}</p>
-                    <p class="text-neutral-content">{{ $comment->created_at->diffForHumans() }}</p>
-                    <p class="text-neutral-content">{{ $comment->user->name }}</p>
-                </div>
-            </div>
-        @endforeach
-    </div>
+@extends('partials.layout') 
+@section('title', $post->title) 
+@section('content') 
+    <div class="container mx-auto"> 
+        @include('partials.post-card') 
+        <a href="./comments/create/{{$post->id}}" class="btn btn-primary">Leave the comment</a> 
+        <h3 class="text-2xl">Comments:</h3> 
+        @foreach($post->comments()->latest()->get() as $comment) 
+            <div class="card bg-base-200 shadow-xl mt-3"> 
+                <div class="card-body"> 
+                    <p>{{ $comment->body }}</p> 
+                    <p class="text-neutral-content">{{ $comment->created_at->diffForHumans() }}</p> 
+                    <p class="text-neutral-content">{{ $comment->user->name }}</p> 
+                </div> 
+            </div> 
+        @endforeach 
+    </div> 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,43 @@
-<?php 
- 
-use App\Http\Controllers\PostController; 
-use App\Http\Controllers\ProfileController; 
-use App\Http\Controllers\PublicController; 
-use App\Http\Controllers\CommentController; 
-use App\Http\Controllers\TagController; 
-use Illuminate\Support\Facades\Route; 
- 
-Route::get('/', [PublicController::class, 'index'])->name('home'); 
-Route::get('/post/{post}', [PublicController::class, 'post'])->name('post'); 
-Route::get('/tag/{tag}', [PublicController::class, 'tag'])->name('tag'); 
-Route::get('/comment/{comment}', [PublicController::class, 'comment'])->name('comment'); 
- 
-// Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index'); 
-// Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create'); 
-// Route::post('/admin/posts', [PostController::class, 'store'])->name('posts.store'); 
-// Route::get('/admin/posts/{post}', [PostController::class, 'show'])->name('posts.show'); 
-// Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit'); 
-// Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update'); 
-// Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy'); 
- 
- 
- 
-Route::get('/dashboard', function () { 
-    return view('dashboard'); 
-})->middleware(['auth', 'verified'])->name('dashboard'); 
- 
-Route::middleware('auth')->group(function () { 
-    Route::resource('/admin/posts', PostController::class); 
-    Route::resource('/admin/comments', CommentController::class); 
- 
-    Route::post('/post/{post}/like', [PublicController::class, 'like'])->name('like'); 
- 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit'); 
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update'); 
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy'); 
-}); 
- 
-require DIR.'/auth.php';
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublicController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\TagController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', [PublicController::class, 'index'])->name('home');
+Route::get('/post/{post}', [PublicController::class, 'post'])->name('post');
+Route::get('/tag/{tag}', [PublicController::class, 'tag'])->name('tag');
+Route::get('/comment/{comment}', [PublicController::class, 'comment'])->name('comment');
+
+// Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
+// Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
+// Route::post('/admin/posts', [PostController::class, 'store'])->name('posts.store');
+// Route::get('/admin/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+// Route::get('/admin/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+// Route::put('/admin/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+// Route::delete('/admin/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('/admin/posts', PostController::class);
+    Route::resource('/admin/comments', CommentController::class);
+    Route::resource('/admin/users', UserController::class);
+
+    Route::post('/post/{post}/like', [PublicController::class, 'like'])->name('like');
+    Route::resource('/post/comments/create/', CommentController::class);
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
