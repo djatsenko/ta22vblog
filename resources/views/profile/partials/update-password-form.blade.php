08e8cd28d28d7a72d49b993ab0312d5ffd8e71b0
djatsenko <vladislav.djatsenko@tptlive.ee>
2025-01-24 14:42:01 +0200

    Issues fix

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,21 +1,19 @@
 <section>
     <header>
-        
         <h2 class="mt-4 text-lg font-medium text-white">
             Update Password
         </h2>
 
-       
         <p class="mt-4 text-sm text-gray-300">
             Ensure your account is using a long, random password to stay secure
         </p>
     </header>
 
-@@ -14,34 +14,38 @@
+    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+        @csrf
         @method('put')
 
         <div>
-            
             <label for="update_password_current_password" class="text-white block">Current Password</label>
             <input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full input input-bordered @error('current_password') input-error @enderror" autocomplete="current-password" />
             @error('current_password')
@@ -24,7 +22,6 @@
         </div>
 
         <div>
-            
             <label for="update_password_password" class="text-white block">New Password</label>
             <input id="update_password_password" name="password" type="password" class="mt-1 block w-full input input-bordered @error('password') input-error @enderror" autocomplete="new-password" />
             @error('password')
@@ -33,7 +30,6 @@
         </div>
 
         <div>
-            
             <label for="update_password_password_confirmation" class="text-white block">Confirm Password</label>
             <input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full input input-bordered @error('password_confirmation') input-error @enderror" autocomplete="new-password" />
             @error('password_confirmation')
@@ -42,16 +38,15 @@
         </div>
 
         <div class="flex items-center gap-4">
-           
             <button type="submit" class="btn btn-primary">
                 <p class="text-white">Save</p>
             </button>
 
             @if (session('status') === 'password-updated')
-                
                 <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-300">
                     Saved
                 </p>
             @endif
         </div>
     </form>
+</section>
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,21 +1,23 @@
 <section>
     <header>
-       
         <h2 class="mt-4 text-lg font-medium text-white-600">
             <p class="text-white">Profile Information</p>
         </h2>
 
-        
         <p class="mt-4 text-sm text-white-600">
             <p class="text-white">Update your account's profile information and email address</p>
         </p>
     </header>
 
-@@ -18,46 +18,49 @@
+    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+        @csrf
+    </form>
+
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+        @csrf
         @method('patch')
 
         <div>
-            
             <label for="name" class="text-white block">Name</label>
             <input id="name" name="name" type="text"
                 class="mt-1 block w-full input input-bordered @error('name') input-error @enderror"
@@ -26,7 +28,6 @@
         </div>
 
         <div>
-            
             <label for="email" class="text-white">Email</label>
             <input id="email" name="email" type="email" class="mt-1 block w-full input input-bordered"
                 value="{{ old('email', $user->email) }}" required autofocus autocomplete="username" />
@@ -34,14 +35,11 @@
                 <span class="text-error mt-2 text-sm">{{ $message }}</span>
             @enderror
 
-            
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
                 <div>
-                   
                     <p class="text-sm mt-2 text-gray-300">
                         Your email address is unverified.
 
-                        
                         <button form="send-verification"
                             class="underline text-sm text-gray-300 hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             Click here to re-send the verification email.
@@ -50,7 +48,6 @@
 
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600">
-                            
                             A new verification link has been sent to your email address.
                         </p>
                     @endif
@@ -59,13 +56,12 @@
         </div>
 
         <div class="flex items-center gap-4">
-          
             <button type="subtim" class="btn btn-primary">Save</button>
 
             @if (session('status') === 'profile-updated')
-                
                 <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
                     class="text-sm text-gray-300">Saved</p>
             @endif
         </div>
     </form>
+</section>
